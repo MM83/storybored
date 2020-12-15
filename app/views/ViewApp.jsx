f0208dfb99c4f87fe142fc5f49611f90def452b0
Nick Stone <njstone1983@gmail.com>
2020-12-15 07:25:18 +0000

    first

--- app/views/ViewApp.jsx
+++ app/views/ViewApp.jsx
@@ -1,61 +1,76 @@
 import React from 'react';
 import $ from 'jquery';
-import DataModel from '../js/DataModel';
-import Commands from '../js/Commands';
-import { Button } from 'react-bootstrap';
-import { HashRouter, Route, Switch } from 'react-router-dom';
-import TopBar from './TopBar';
-import NavPanel from './NavPanel';
-import ViewStory from './ViewStory';
-import ViewNotes from './ViewNotes';
-import ViewCharacters from './ViewCharacters';
-import ViewEvents from './ViewEvents';
-import ViewLocations from './ViewLocations';
-import ViewItems from './ViewItems';
-import ViewRegions from './ViewRegions';
-import ViewAttributes from './ViewAttributes';
-import ViewWorld from './ViewWorld';
-import ViewModal from './ViewModal';
+import { Navbar, Nav, NavDropdown, Button } from 'react-bootstrap';
+// import DataModel from '../js/DataModel';
+// import Commands from '../js/Commands';
+// import { Button } from 'react-bootstrap';
+// import { HashRouter, Route, Switch } from 'react-router-dom';
+// import TopBar from './TopBar';
+// import NavPanel from './NavPanel';
+// import ViewStory from './ViewStory';
+// import ViewNotes from './ViewNotes';
+// import ViewCharacters from './ViewCharacters';
+// import ViewEvents from './ViewEvents';
+// import ViewLocations from './ViewLocations';
+// import ViewItems from './ViewItems';
+// import ViewRegions from './ViewRegions';
+// import ViewAttributes from './ViewAttributes';
+// import ViewWorld from './ViewWorld';
+// import ViewModal from './ViewModal';
 
+export default function ViewApp(props)
+{
+  return (<div>
+    <Navbar bg="light" expand="lg">
+  <Navbar.Brand href="#home">StoryBored</Navbar.Brand>
+    <Nav>
+      <NavDropdown title="Project">
+        <NavDropdown.Item>Save</NavDropdown.Item>
+        <NavDropdown.Item>Reload</NavDropdown.Item>
+        <NavDropdown.Divider />
+        <NavDropdown.Item>Branch...</NavDropdown.Item>
+        <NavDropdown.Item>Tree...</NavDropdown.Item>
+        <NavDropdown.Divider />
+        <NavDropdown.Item>New...</NavDropdown.Item>
+      </NavDropdown>
 
-class ViewApp extends React.Component {
+      <NavDropdown title="Elements">
+        <NavDropdown.Item>Save</NavDropdown.Item>
+        <NavDropdown.Item>Reload</NavDropdown.Item>
+        <NavDropdown.Divider />
+        <NavDropdown.Item>Branch...</NavDropdown.Item>
+        <NavDropdown.Item>Tree...</NavDropdown.Item>
+        <NavDropdown.Divider />
+        <NavDropdown.Item>New...</NavDropdown.Item>
+      </NavDropdown>
 
-  constructor(props)
-  {
-    super(props);
-    //<Route path="/home" component={ViewHome}/>
-  }
+    </Nav>
 
+</Navbar>
 
-
-  render() {
-      return (
-        <div className="main-app">
-          <TopBar/>
-          <div className="main-panel">
-            <NavPanel/>
-            <HashRouter>
-              <div className="main-panel-column">
-                <Switch>
-                  <Route path="/synopsis" component={ViewStory}/>
-                  <Route path="/notes" component={ViewNotes}/>
-                  <Route path="/characters" component={ViewCharacters}/>
-                  <Route path="/locations" component={ViewLocations}/>
-                  <Route path="/events" component={ViewEvents}/>
-                  <Route path="/items" component={ViewItems}/>
-                  <Route path="/regions" component={ViewRegions}/>
-                  <Route path="/attributes" component={ViewAttributes}/>
-                  <Route path="/world" component={ViewWorld}/>
-                </Switch>
-              </div>
-            </HashRouter>
-          </div>
-          <ViewModal/>
-        </div>
-
-      );
-  }
-
+  </div>);
 }
 
-export default ViewApp;
+// class ViewApp extends React.Component {
+//
+//   constructor(props)
+//   {
+//     super(props);
+//     //<Route path="/home" component={ViewHome}/>
+//   }
+//
+//
+//
+//   render() {
+//       return (
+//         <div className="main-app">
+//           <TopBar/>
+//
+//         </div>
+//
+//       );
+//   }
+//
+// }
+//
+// export default ViewApp;
